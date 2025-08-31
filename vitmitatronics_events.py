f4bd51eeccafe6abfb515dd64eb8368a79232e41
MarvelMathesh <123v.mathesh@gmail.com>
2025-08-31 22:49:47 +0530

    chore(ui): initial SquareLine Studio LVGL UI import

---
+++ vitmitatronics_events.py
@@ -0,0 +1,52 @@
+
+def homeConfigClicked(event_struct):
+    return
+
+
+def chooseButton1Clicked(event_struct):
+    return
+
+
+def chooseButton2Clicked(event_struct):
+    return
+
+
+def chooseBackButtonClicked(event_struct):
+    return
+
+
+def paymentBackButtonClicked(event_struct):
+    return
+
+
+def preparingCancelButtonClicked(event_struct):
+    return
+
+
+def successPanelButtonClicked(event_struct):
+    return
+
+
+def configButtonEspressoClicked(event_struct):
+    return
+
+
+def configButtonCoffeeClicked(event_struct):
+    return
+
+
+def configButtonResetClicked(event_struct):
+    return
+
+
+def configButtonBackClicked(event_struct):
+    return
+
+
+def configButtonRestartClicked(event_struct):
+    return
+
+
+def configPasswordKeyboardValueChanged(event_struct):
+    return
+
